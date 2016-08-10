1567e8b06ed822ff998c68f60df2247ae107527a
zhaozhuo <zhaozhuoboy@163.com>
2016-08-10 09:56:59 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render () {
+    return(
+      <div>
+          hello
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
